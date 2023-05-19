4e520a2e5b772544e78affaabfee6b4d4b77d5b9
pramodyasahan <sahandil1013@gmail.com>
2023-05-19 20:31:49 +0530

    feat: upgrade code structure with Class and methods

--- main.py
+++ main.py
@@ -1,112 +1,88 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "milk": 0,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+class CoffeeMachine:
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+            "money": 0,
+        }
+        self.menu = {
+            "espresso": {
+                "ingredients": {
+                    "water": 50,
+                    "milk": 0,
+                    "coffee": 18,
+                },
+                "cost": 1.5,
+            },
+            "latte": {
+                "ingredients": {
+                    "water": 200,
+                    "milk": 150,
+                    "coffee": 24,
+                },
+                "cost": 2.5,
+            },
+            "cappuccino": {
+                "ingredients": {
+                    "water": 250,
+                    "milk": 100,
+                    "coffee": 24,
+                },
+                "cost": 3.0,
+            }
+        }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-    "money": 0,
-}
+    def report(self):
+        print(f"    Water: {self.resources['water']}ml")
+        print(f"    Milk: {self.resources['milk']}ml")
+        print(f"    Coffee: {self.resources['coffee']}g")
+        print(f"    Money: ${self.resources['money']}")
 
+    def check_resources(self, coffee):
+        for item, amount in self.menu[coffee]["ingredients"].items():
+            if self.resources[item] < amount:
+                print(f"Sorry there is not enough {item}")
+                return False
+        return True
 
-def check_resources(coffee):
-    sufficient = bool
-    for key_value in resources.keys():
-        for x in range(3):
-            if resources[key_value] < MENU[coffee]["ingredients"][key_value]:
-                sufficient = False
-                return key_value, sufficient
-        break
-    sufficient = True
-    return None, sufficient
+    def manage_resources(self, coffee):
+        for item, amount in self.menu[coffee]["ingredients"].items():
+            self.resources[item] -= amount
 
+    def process_coins(self):
+        print("Please insert coins.")
+        total = int(input("    How many quarters? ")) * 0.25
+        total += int(input("    How many dimes? ")) * 0.10
+        total += int(input("    How many nickles? ")) * 0.05
+        total += int(input("    How many pennies? ")) * 0.01
+        return total
 
-def manage_resources(coffee):
-    count = 0
-    for value in resources.keys():
-        resources[value] = resources[value] - MENU[coffee]["ingredients"][value]
-        count += 1
-        if count == 3:
-            break
-
-
-def report():
-    print(f"    Water: {resources['water']}ml")
-    print(f"    Milk: {resources['milk']}ml")
-    print(f"    Coffee: {resources['coffee']}g")
-    print(f"    Money: ${resources['money']}")
-
-
-def serve_coffee(coffee):
-    resources['money'] += MENU[coffee]['cost']
-    change = total - MENU[coffee]['cost']
-    print(f"Here is ${round(change, 2)} in change.")
-    print(f"Here is your {coffee} ☕️. Enjoy!")
-    manage_resources(coffee)
-
-
-start = True
-while start:
-    total = 0
-    request = input("What would you like? (espresso/latte/cappuccino): ")
-
-    while request.lower() == "report":
-        report()
-        request = input("What would you like? (espresso/latte/cappuccino): ")
-
-    else:
-        result_key, result_bool = check_resources(request.lower())
-
-        if request.lower() == "off":
-            break
-
-        if not result_bool:
-            print(f"Sorry there is not enough {result_key}")
+    def serve_coffee(self, coffee):
+        total = self.process_coins()
+        if total >= self.menu[coffee]['cost']:
+            change = total - self.menu[coffee]['cost']
+            print(f"Here is ${round(change, 2)} in change.")
+            self.resources['money'] += self.menu[coffee]['cost']
+            print(f"Here is your {coffee} ☕️. Enjoy!")
+            self.manage_resources(coffee)
         else:
-            print("Please insert coins.")
-            total += int(input("    How many quarters: ")) * 0.25
-            total += int(input("    How many dimes: ")) * 0.10
-            total += int(input("    How many nickles: ")) * 0.05
-            total += int(input("    How many pennies: ")) * 0.01
+            print("Sorry that's not enough money.Money refunded.")
 
-            if request.lower() == "espresso":
-                if total >= MENU[request.lower()]['cost']:
-                    serve_coffee("espresso")
-                else:
-                    print("Sorry that's not enough money. Money refunded.")
+    def run(self):
+        while True:
+            request = input("\nWhat would you like? (espresso/latte/cappuccino): ").lower()
+            if request == "report":
+                self.report()
+            elif request in self.menu.keys():
+                if self.check_resources(request):
+                    self.serve_coffee(request)
+            elif request == "off":
+                break
+            else:
+                print("Invalid input. Please enter espresso, latte or cappuccino.")
 
-            elif request.lower() == "latte":
-                if total >= MENU[request.lower()]['cost']:
-                    serve_coffee("latte")
-                else:
-                    print("Sorry that's not enough money. Money refunded.")
 
-            elif request.lower() == "cappuccino":
-                if total >= MENU[request.lower()]['cost']:
-                    serve_coffee("cappuccino")
-                else:
-                    print("Sorry that's not enough money. Money refunded.")
+# CreatingCoffee Machine Instance
+coffeeMachine = CoffeeMachine()
+coffeeMachine.run()
